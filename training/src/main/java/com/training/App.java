4523b3fef54a71709fc81be6d5eddbbbad1e6448
nikhila <nikhila@nikhila-PC>
2014-05-24 14:53:37 -0700

    commit project 1

--- training/src/main/java/com/training/App.java
+++ training/src/main/java/com/training/App.java
@@ -6,6 +6,7 @@ package com.training;
  */
 public class App 
 {
+	// Eclipse is quite working well
     public static void main( String[] args )
     {
         System.out.println( "Hello World! commit " );
